8396fe734d958f40dfcb3f97ca4e6a28b90e1811
l1mit <BrokenRing31@gmail.com>
2025-01-24 23:53:36 +0800

    [BACKUP] Backup the code

--- backend/router/router.go
+++ backend/router/router.go
@@ -2,6 +2,7 @@ package router
 
 import (
 	"gin_demo/controller"
+	"gin_demo/middleware"
 
 	"github.com/gin-gonic/gin"
 )
@@ -9,7 +10,7 @@ import (
 func CollectRtoutes(r *gin.Engine) *gin.Engine {
 	r.POST("/api/auth/register", controller.Register)
 	r.POST("/api/auth/login", controller.Login)
-	// r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info)
+	r.GET("/api/auth/info", middleware.AuthMiddleware(), controller.Info)
 
 	return r
 }

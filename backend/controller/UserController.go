387eb0b7cde965c3e1d2d18412ee9e10d76f4bba
l1mit <BrokenRing31@gmail.com>
2025-01-25 01:37:07 +0800

    [ADD] Add DTO for data transfer and encapsulation
    - Introduced a new DTO (Data Transfer Object) to standardize data transfer
    - Encapsulated request/response data for better separation of concerns

--- backend/controller/UserController.go
+++ backend/controller/UserController.go
@@ -5,7 +5,9 @@ import (
 	"net/http"
 
 	"gin_demo/common"
+	"gin_demo/dto"
 	"gin_demo/model"
+	"gin_demo/response"
 	"gin_demo/util"
 
 	"github.com/gin-gonic/gin"
@@ -36,13 +38,15 @@ func Register(c *gin.Context) {
 
 	// 验证手机号长度
 	if len(telephone) != 11 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		response.Response(c, http.StatusUnprocessableEntity, 422, nil, "手机号必须为11位")
+		// c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
 		return
 	}
 
 	// 验证密码长度
 	if len(password) < 8 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于8位"})
+		response.Response(c, http.StatusUnprocessableEntity, 422, nil, "密码不能少于8位")
+		// c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于8位"})
 		return
 	}
 
@@ -56,7 +60,8 @@ func Register(c *gin.Context) {
 	//判断手机号是否存在
 
 	if isTelephoneExist(db, telephone) {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "该手机号已注册用户"})
+		response.Response(c, http.StatusUnprocessableEntity, 422, nil, "该手机号已注册用户")
+		// c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "该手机号已注册用户"})
 		return
 	}
 
@@ -66,8 +71,9 @@ func Register(c *gin.Context) {
 	hashPassword, err := bcrypt.GenerateFromPassword([]byte(password), bcrypt.DefaultCost)
 
 	if err != nil {
-		// TODO 稍后要把这段反馈修改为其他的理由
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 500, "msg": "加密错误"})
+
+		response.Response(c, http.StatusUnprocessableEntity, 500, nil, "系统错误")
+		// c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 500, "msg": "加密错误"})
 	}
 
 	newUser := model.User{
@@ -79,9 +85,11 @@ func Register(c *gin.Context) {
 	db.Create(&newUser)
 
 	// 返回结果
-	c.JSON(200, gin.H{
-		"code": "200",
-		"msg":  "注册成功"})
+	// c.JSON(200, gin.H{
+	// 	"code": "200",
+	// 	"msg":  "注册成功"})
+
+	response.Success(c, nil, "注册成功")
 }
 
 func Login(c *gin.Context) {
@@ -96,13 +104,15 @@ func Login(c *gin.Context) {
 
 	// 验证手机号长度
 	if len(telephone) != 11 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
+		response.Response(c, http.StatusUnprocessableEntity, 422, nil, "手机号必须为11位")
+		// c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "手机号必须为11位"})
 		return
 	}
 
 	// 验证密码长度
 	if len(password) < 8 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于8位"})
+		response.Response(c, http.StatusUnprocessableEntity, 422, nil, "密码不能少于8位")
+		// c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "密码不能少于8位"})
 		return
 	}
 
@@ -112,13 +122,15 @@ func Login(c *gin.Context) {
 	db.Where("telephone = ?", telephone).First(&user)
 
 	if user.ID == 0 {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
+		response.Response(c, http.StatusUnprocessableEntity, 422, nil, "用户不存在")
+		// c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 422, "msg": "用户不存在"})
 		return
 	}
 
 	// 判断密码是否正确
 	if err := bcrypt.CompareHashAndPassword([]byte(user.Password), []byte(password)); err != nil {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 400, "msg": "密码错误"})
+		response.Response(c, http.StatusUnprocessableEntity, 400, nil, "密码错误")
+		// c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 400, "msg": "密码错误"})
 		return
 	}
 
@@ -127,23 +139,29 @@ func Login(c *gin.Context) {
 	token, err := common.ReleaseToken(user)
 
 	if err != nil {
-		c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 500, "msg": "系统异常"})
+		response.Response(c, http.StatusUnprocessableEntity, 500, nil, "系统异常")
+		// c.JSON(http.StatusUnprocessableEntity, gin.H{"code": 500, "msg": "系统异常"})
 		log.Printf("token generate error: %v", err)
 		return
 	}
 
 	// 返回结果
-	c.JSON(200, gin.H{
-		"code": "200",
-		"data": gin.H{"token": token},
-		"msg":  "登录成功"})
+	// c.JSON(200, gin.H{
+	// 	"code": "200",
+	// 	"data": gin.H{"token": token},
+	// 	"msg":  "登录成功"})
+
+	response.Success(c, gin.H{"token": token}, "登录成功")
 }
+
+// 返回账户信息
 
 func Info(c *gin.Context) {
 	user, _ := c.Get("user")
 
+	// ? 使用DTO来封装传输 来确保不会有不相关的敏感信息也被返回
 	c.JSON(200, gin.H{
 		"code": "200",
-		"data": gin.H{"user": user}})
+		"data": gin.H{"user": dto.ToUserDto(user.(model.User))}})
 
 }
